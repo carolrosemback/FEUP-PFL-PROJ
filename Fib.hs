ba4300dcc5bcf689e4b687bb8b0563918fe0e651
Margarida Cosme <margaridacosme99@gmail.com>
2021-11-12 12:19:25 +0000

    Fib

--- Fib.hs
+++ Fib.hs
@@ -1,8 +1,18 @@
 fibRec :: (Integral a) => a -> a
-fibRec 0 = 0
-fibRec 1 = 1
-fibRec x = fibRec (x-1) + fibRec(x-2)
+fibRec n
+    | n == 0 = 0 
+    | n == 1 = 1
+    | otherwise = fibRec (n-1) + fibRec(n-2)
 
-fibLista :: (Integral a) => a -> a
-fibLista n = fibs !! n
+fibLista :: (Integral a) => Int -> a
+fibLista i = (fibLista' i [0,1])!!i
+
+fibLista':: (Integral a) => Int->[a]->[a] 
+fibLista' n l
+    | n < 2 = l
+    | n > 1 = fibLista' n-1 + fibLista' l++[l!!(n-1) + l!!(n-2)]
+
+fibListaInfinita :: (Integral a) => Int -> a
+fibListaInfinita i = fibs!!i
     where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
