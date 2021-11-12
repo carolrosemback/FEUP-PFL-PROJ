5681fb434391b1abe8d0ed47ca51893622bfbaa4
Margarida Cosme <margaridacosme99@gmail.com>
2021-11-12 12:36:19 +0000

    FibLista feito

--- Fib.hs
+++ Fib.hs
@@ -5,12 +5,14 @@ fibRec n
     | otherwise = fibRec (n-1) + fibRec(n-2)
 
 fibLista :: (Integral a) => Int -> a
-fibLista i = (fibLista' i [0,1])!!i
+fibLista i = (fibLista' i 1 [0,1])!!i
 
-fibLista':: (Integral a) => Int->[a]->[a] 
-fibLista' n l
+fibLista':: (Integral a) => Int->Int->[a]->[a] 
+fibLista' n c l
     | n < 2 = l
-    | n > 1 = fibLista' n-1 + fibLista' l++[l!!(n-1) + l!!(n-2)]
+    | n == c = l
+    | n > c = fibLista' n (c+1) (l ++ [l!!c + l!!(c-1)])
+
 
 fibListaInfinita :: (Integral a) => Int -> a
 fibListaInfinita i = fibs!!i
